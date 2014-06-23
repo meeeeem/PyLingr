3ef710f34480dd6f46247d22549d8839aac1408c
meeeeem <punipuniomochi@gmail.com>
2014-06-23 22:02:50 +0900

    update lingr_shell.py

--- LingrShell/lingr_shell.py
+++ LingrShell/lingr_shell.py
@@ -1,35 +1,37 @@
 # -*-  coding:utf-8 -*-
 
-import sys, re, subprocess, random, os
+import sys, re, subprocess, random, os, json
 from getpass import getpass
 from cmd import Cmd
 from banner import banner
 sys.path.append(os.getcwd())
 sys.path.append(os.path.dirname(__file__))
-import config
 
 from lingr import Lingr
+
+CONF_PATH = os.path.abspath(os.path.dirname(__file__) + '/config')
+
 
 class LingrShell(Cmd):
     def __init__(self):
         Cmd.__init__(self)
 
-        '''
-        self.username = config.username
-        self.password = config.password
+        user_list = {}
+        try:
+            with open(CONF_PATH, 'r') as f:
+                json_obj = json.load(f)
 
-        if config.username is '' or config.password is '':
-            self.username, self.password = conf()
+            for i, u in enumerate(json_obj.keys()):
+                user_list.update({i+1: u})
+            login_user = select_item(user_list, 'user')
+            username = login_user
+            password = json_obj[login_user]
+        except:
+            print('can not find login info.')
+            username, password = None, None
 
-        self.lingr = Lingr(self.username, self.password)
-        data = self.lingr.create_session()
-        if data['status'] == 'error':
-            print('login settins is error.')
-            sys.exit()
-        self.rooms = {}
-        '''
+        self.lingr, self.username, self.password = login(username, password)
 
-        self.lingr, self.username, self.password = login(config.username, config.password)
         self.rooms = {}
 
         for i, r in enumerate(self.lingr.get_rooms()['rooms']):
@@ -39,7 +41,7 @@ class LingrShell(Cmd):
 
         print(random.choice(banner))
 
-        self.now_room = select_room(self.rooms)
+        self.now_room = select_item(self.rooms, 'room')
 
         if self.now_room is not None:
             self.prompt = '(Lingr) {username}@{room} >>> '.format(username=self.username, room=self.now_room)
@@ -59,31 +61,30 @@ class LingrShell(Cmd):
 
     def do_chroom(self, key):
         'select room your joined: CHROOM <room>'
-        if key == '' or int(key) not in self.rooms.keys():
-            self.now_room = select_room(self.rooms)
+        if key == '' or key not in str(self.rooms.keys()):
+            self.now_room = select_item(self.rooms, 'room')
         else:
             self.now_room = self.rooms[int(key)]
         self.prompt = '(Lingr) {username}@{room} >>> '.format(username=self.username, room=self.now_room)
 
     def do_say(self, text):
-        'post to joined room: SAY <text>'
+        'post <text> to joined room: SAY <text>'
         self.lingr.say(self.now_room, text)
 
-    def do_chusr(self, dummy):
-        'Change login user: CHUSR'
+    def do_hi(self, dummy):
+        'post "hi" to joined room: HI'
+        self.lingr.say(self.now_room, 'hi')
+
+    def do_chuser(self, username):
+        'Change login user: CHUSER'
 
         self.lingr.destroy_session()
-        self.username, self.password = conf()
-        #self.lingr = Lingr(self.username, self.password)
-        #data = self.lingr.create_session()
-        self.username, self.password = login()
-
+        self.lingr, self.username, self.password = login(username=username)
         self.rooms = {}
         for i, r in enumerate(self.lingr.get_rooms()['rooms']):
             self.rooms.update({i+1: r})
-        print(self.rooms)
 
-        self.now_room = select_room(self.rooms)
+        self.now_room = select_item(self.rooms, 'room')
         self.prompt = '(Lingr) {username}@{room} >>> '.format(username=self.username, room=self.now_room)
 
     def do_bye(self, dummy):
@@ -91,21 +92,31 @@ class LingrShell(Cmd):
         print('bye')
         sys.exit()
 
+
 def conf():
-        username = input('username:')
-        password = getpass('password:')
+    username = input('username:')
+    password = getpass('password:')
 
-        settings = "username = '{username}'\npassword = '{password}'".format(username=username, password=password)
+    try:
+        with open(CONF_PATH) as f:
+            user_list = json.load(f)
+    except:
+        user_list = {}
 
-        with open(os.path.dirname(__file__) + '/config.py', 'w') as f:
-            f.write(settings)
+    user_list.update({username: password})
 
+    with open(CONF_PATH, 'w') as f:
+        f.write(json.dumps(user_list))
 
-        return username, password
+    return username, password
 
-
-def login(username, password):
+def login(username=None, password=None):
     while True:
+        if username is not None:
+            with open(CONF_PATH) as f:
+                json_obj = json.load(f)
+            password = json_obj.get(username, None)
+
         l = Lingr(username, password)
         data = l.create_session()
 
@@ -113,23 +124,22 @@ def login(username, password):
             print('login complete!')
             return l, username, password
         else:
-            print('cannot login... please set to login.')
             print('*login setting*')
             username, password = conf()
             del l
 
-def select_room(rooms):
-    print('Please select room from below.')
-    for k, v in rooms.items():
+def select_item(dict_item, flag):
+    print('print select {flag} from below'.format(flag=flag))
+    for k, v in dict_item.items():
         print('{key}: {value}'.format(key=k, value=v))
 
     while True:
-        key = input('(Lingr) Input room number >>> ')
+        key = input('(Lingr) Input {flag} number >>> '.format(flag=flag))
         if key == 'bye':
             sys.exit()
         elif re.search('\d+', key):
-            if int(key) in rooms.keys():
-                return rooms[int(key)]
+            if int(key) in dict_item.keys():
+                return dict_item[int(key)]
 
 if __name__ == '__main__':
     LingrShell().cmdloop()
